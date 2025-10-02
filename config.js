cef571b5b878f138051cd4d72a5a6f2bb7d72c07
Angelithoxz <ryuseiclubxyz@gmail.com>
2025-10-01 20:12:59 -0500

    empieza la chamba :v

--- config.js
+++ config.js
@@ -5,57 +5,40 @@ import fs from 'fs'
 import cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import axios from 'axios'
-import moment from 'moment-timezone' 
+import moment from 'moment-timezone'
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-// Staff De CrowBot
+//*─ׄ─ׅ─⭒─ׄ─ INFO PROPIETARIO ─ׄ─⭒─*
 global.owner = [
-   ['50557865603', 'Creador WillZek💛', true],
-   ['5212731595017', 'CrowBot Soporte', true],
-   ['584148256527', 'Dev Diomar', true],
-   ['584120346669', 'Dev ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜', true],
-   ['584241836217', 'Dev Prak', true],
-   ['51968382008', 'Dev Darkcore', true],
+   ['51901019299', '✦ 𝐀𝐧𝐠𝐞𝐥𝐢𝐭𝐡𝐨𝐱𝐲𝐳 ✦', true],
 ]
 
 global.creadorbot = [
-   ['50557865603', 'WillZek', true],
+   ['51901019299', '✦ 𝐀𝐧𝐠𝐞𝐥𝐢𝐭𝐡𝐨𝐱𝐲𝐳 ✦', true],
 ]
-  
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.mods = ['']
+global.prems = ['']
 
-global.mods = ['50368471855', '51920227615', '5219831715910', '573007796996', '50488198573', '51991055096', '5212731590195', '50557865603', '595975677765', '584245610338']
-
-global.prems = ['573215751237']
-
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+//*─ׄ─ׅ─⭒─ׄ─ INFO BOT ─ׄ─⭒─*
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.16' 
 global.vs = '2.2.0'
-global.nameqr = 'CrowBot-MF'
-global.namebot = 'CrowBot-MF'
-global.sessions = 'CrowSession'
-global.jadi = 'CrowJadiBot' 
-global.yukiJadibts = true // Activado
+global.nameqr = 'BocchiBot'
+global.namebot = '✦ Bocchi Hitori ✦'
+global.sessions = 'BocchiSession'
+global.jadi = 'BocchiJadiBot' 
+global.yukiJadibts = true 
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.packname = '✦ Bocchi Hitori ✦ ❖'
+global.botname = '✧ Bocchi Hitori ✧'
+global.wm = '✦ Ryusei Club Infinity ☆'
+global.author = '♥︎ Made By Ryusei Club Infinity ♥︎'
+global.dev = '© Ryusei Club Infinity'
+global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 un momento, procesando tu pedido ✰'
+global.textbot = `「 ✦ Bocchi Hitori ✦ 」`
+global.publi = '✦ Sigue el canal oficial 👇'
 
-global.packname = '【✧】𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐌.𝐅 ❖'
-global.botname = '(✦◠‿◠)𝑪𝒓𝒐𝒘𝑩𝒐𝒕-𝑴𝑭'
-global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🍨★ ׄ ꒱ 𝑪𝒓𝒐𝒘𝑩𝒐𝒕 - 𝑴.𝑭 ୭'
-global.author = '(⁎˃ᴗ˂⁎)𝐌𝐚𝐝𝐞 𝐁𝐲 𝐖𝐢𝐥𝐥𝐙𝐞𝐤𓆪'
-global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ m᥆᥆ᥒ-𝖿᥆rᥴᥱ'
-global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨𝐬 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
-global.namebot = 'ᥴr᥆ᥕᑲ᥆𝗍 s𝗍'
-global.textbot = `「 🔱 𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐌𝐅 🔱 」`
-global.vs = '2.2.0'
-global.publi = '✰𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+//*─ׄ─ׅ─⭒─ׄ─ IMÁGENES ─ׄ─⭒─*
 global.imagen1 = fs.readFileSync('./media/menus/Menu.jpg')
 global.imagen2 = fs.readFileSync('./media/menus/Menu2.jpg')
 global.imagen3 = fs.readFileSync('./media/menus/Menu3.jpg')
@@ -63,32 +46,43 @@ global.welcome = fs.readFileSync('./media/welcome.jpg')
 global.adios = fs.readFileSync('./media/adios.jpg')
 global.catalogo = fs.readFileSync('./media/catalogo.jpg')
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-global.repobot = 'https://github.com/WillZek/CrowBot-ST'
-global.grupo = 'https://chat.whatsapp.com/B7POPrlJiZf7UWHJKvzVkx'
-global.gteam = 'https://chat.whatsapp.com/EpkR7XS0hrMDYk89yw57HZ'
-global.gsupport = 'https://chat.whatsapp.com/K5GLB5CXp3iAl4XsDzav1W'
-global.comu = 'https://chat.whatsapp.com/Bgfb6Ag13BDCvwzOtldlCm'
-global.channel = 'https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W'
-global.insta = 'https://www.instagram.com/crowbot-wa'
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+//*─ׄ─ׅ─⭒─ׄ─ LINKS ─ׄ─⭒─*
+global.repobot = 'https://github.com/'
+global.grupo = ''
+global.gsupport = ''
+global.channel = ''
+global.insta = ''
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '👑【✫𝚃𝙴𝙰𝙼  乂 𝚂𝚃𝙰𝚁𝙲𝙾𝚁𝙴✫】🎩', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+//*─ׄ─ׅ─⭒─ׄ─ ESTILO MENSAJE ─ׄ─⭒─*
+global.estilo = { 
+  key: {  
+    fromMe: false, 
+    participant: `0@s.whatsapp.net`, 
+    ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) 
+  }, 
+  message: { 
+    orderMessage: { 
+      itemCount : -999999, 
+      status: 1, 
+      surface : 1, 
+      message: '✦ Ryusei Club Infinity ✦', 
+      orderTitle: 'BocchiBot', 
+      thumbnail: catalogo, 
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+}
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+//*─ׄ─ׅ─⭒─ׄ─ LIBRERÍAS ─ׄ─⭒─*
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
 global.moment = moment        
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.multiplier = 69 
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+//*─ׄ─ׅ─⭒─ׄ─ AUTO UPDATE ─ׄ─⭒─*
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
