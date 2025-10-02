c16c0555ac66a1f8523fb33116f95d55f1da7ed6
Angelithoxz <ryuseiclubxyz@gmail.com>
2025-10-02 10:49:09 -0500

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -15,14 +15,14 @@ let muptime = clockString(_muptime)
 const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
 const used = process.memoryUsage()
-let Crow = `╭─⬣「 *Estado De CrowBot* 」⬣\n`
-Crow += `│ 💛 *Creador ∙* WillZek\n`
-Crow += `│ 📚 *Grupos Unidos ∙* ${groupsIn.length}\n`
-Crow += `│ 👤 *Chats Privados ∙* ${chats.length - groupsIn.length}\n`
-Crow += `│ 💬 *Total De Chats ∙* ${chats.length}\n`
-Crow += `│ 💛 *Usuarios Registrados ∙* ${totalreg}\n`
-Crow += `│ 🍭 *Grupos Registrados ∙* ${totalchats}\n`
-Crow += `│ 🕜 *Actividad ∙* ${muptime}\n`
+let Crow = `╭─⬣「 *Estado De Bocchi* 」⬣\n`
+Crow += `│ ✦ *Creador ∙* Angelithoxyz\n`
+Crow += `│ ☆ *Grupos Unidos ∙* ${groupsIn.length}\n`
+Crow += `│ ✦ *Chats Privados ∙* ${chats.length - groupsIn.length}\n`
+Crow += `│ ♥︎ *Total De Chats ∙* ${chats.length}\n`
+Crow += `│ ☆ *Usuarios Registrados ∙* ${totalreg}\n`
+Crow += `│ ✦ *Grupos Registrados ∙* ${totalchats}\n`
+Crow += `│ ♥︎ *Actividad ∙* ${muptime}\n`
 Crow += `╰─⬣`
 await conn.sendFile(m.chat, pp, 'nino.jpg', Crow, fkontak, null, rcanal)
 }
